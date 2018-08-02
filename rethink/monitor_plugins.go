6521a892098e14366357db2efd40f6e82b301729
themanzi <christophermmanzi@protonmail.com>
2018-08-02 17:22:20 -0500

    added plugin monitor

--- rethink/monitor_plugins.go
+++ rethink/monitor_plugins.go
@@ -1,1 +1,39 @@
 package rethink
+
+import (
+	"log"
+
+	r "gopkg.in/gorethink/gorethink.v4"
+)
+
+func watchChanges(res *r.Cursor) <-chan interface{} {
+	out := make(chan interface{})
+	go func(cursor *r.Cursor) {
+		var doc interface{}
+		for cursor.Next(&doc) {
+			log.Printf("Change: %v", doc)
+			out <- doc
+		}
+	}(res)
+	return out
+}
+
+func MonitorPlugins() (<-chan interface{}, <-chan error) {
+	outErr := make(chan error)
+
+	session, err := r.Connect(r.ConnectOpts{
+		Address: "127.0.0.1",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	res, err := r.DB("Controller").Table("Plugins").Changes().Run(session)
+	if err != nil {
+		panic(err)
+	}
+
+	outDB := watchChanges(res)
+
+	return outDB, outErr
+}
