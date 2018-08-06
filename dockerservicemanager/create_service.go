9933371a308351b875b5505192ada2b1855781d6
themanzi <christophermmanzi@protonmail.com>
2018-08-06 13:41:31 -0500

    get env

--- dockerservicemanager/create_service.go
+++ dockerservicemanager/create_service.go
@@ -45,6 +45,9 @@ type PluginServiceConfig struct {
 func getTagFromEnv() string {
 	temp := os.Getenv("TAG")
 	if temp == "" {
+		temp = os.Getenv("TRAVIS_BRANCH")
+	}
+	if temp == "" {
 		temp = "latest"
 	}
 	return temp
