4a0616eddf4ba7839028cd4cb509a916e49ffad5
themanzi <christophermmanzi@protonmail.com>
2018-08-17 14:11:38 -0500

    added coverage script

---
+++ codecoverage.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+./cc-test-reporter before-build 
+for pkg in $(go list ./... | grep -v main); do
+    go test -v -parallel 1 -coverprofile=$(echo $pkg | tr / -).cover $pkg
+done
+echo "mode: set" > c.out
+grep -h -v "^mode:" ./*.cover >> c.out
+rm -f *.cover
+
+./cc-test-reporter after-build
