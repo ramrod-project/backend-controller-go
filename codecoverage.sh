f9f17f50a78cd02483571cd9bb27035fb328dbe2
themanzi <christophermmanzi@protonmail.com>
2018-08-21 15:44:56 -0500

    fixed script bash

--- codecoverage.sh
+++ codecoverage.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 ./cc-test-reporter before-build 
 for pkg in $(go list ./... | grep -v main); do
-    if [ $pkg = "test" ]; then
+    if [[ $pkg == "test" ]]; then
         continue
     fi
     go test -v -parallel 1 -coverprofile=$(echo $pkg | tr / -).cover $pkg
