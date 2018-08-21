64125a3477ad41147b4709358557bc0e17ae876a
themanzi <christophermmanzi@protonmail.com>
2018-08-21 15:42:39 -0500

    script fixed

--- codecoverage.sh
+++ codecoverage.sh
@@ -2,6 +2,9 @@
 
 ./cc-test-reporter before-build 
 for pkg in $(go list ./... | grep -v main); do
+    if [ $pkg = "test" ]; then
+        continue
+    fi
     go test -v -parallel 1 -coverprofile=$(echo $pkg | tr / -).cover $pkg
 done
 echo "mode: set" > c.out
