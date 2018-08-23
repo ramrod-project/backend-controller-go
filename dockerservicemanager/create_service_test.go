72ab39ef3e4e8709e941c7a16d0c62f9fbaf6f66
lhitch <lhitch.dev@gmail.com>
2018-08-23 12:36:57 -0500

    create_service_test uses GetManagerIP instead of getManagerIP

--- dockerservicemanager/create_service_test.go
+++ dockerservicemanager/create_service_test.go
@@ -409,7 +409,7 @@ func Test_getTagFromEnv(t *testing.T) {
 	os.Setenv("TAG", oldEnvTag)
 }
 
-func Test_getManagerIP(t *testing.T) {
+func Test_GetManagerIP(t *testing.T) {
 
 	ctx := context.Background()
 	dockerClient, err := client.NewEnvClient()
@@ -432,8 +432,8 @@ func Test_getManagerIP(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := getManagerIP(); got != tt.want {
-				t.Errorf("getManagerIP() = %v, want %v", got, tt.want)
+			if got := GetManagerIP(); got != tt.want {
+				t.Errorf("GetManagerIP() = %v, want %v", got, tt.want)
 			}
 		})
 	}
