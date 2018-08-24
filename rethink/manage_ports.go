c65b2d6df0333993e476ba52e94446341ee5a709
lhitch <lhitch.dev@gmail.com>
2018-08-24 14:24:24 -0500

    use rethink host for ip

--- rethink/manage_ports.go
+++ rethink/manage_ports.go
@@ -11,7 +11,7 @@ import (
 
 func GetIPFromID(servID string) (string, error) {
 	session, err := r.Connect(r.ConnectOpts{
-		Address: "127.0.0.1",
+		Address: GetRethinkHost(),
 	})
 	if err != nil {
 		return "", err
