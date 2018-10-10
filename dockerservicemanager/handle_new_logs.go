df0028339da7d798d6155ef1cd2ea8a6f0f9e6c3
AlexT <alexander.torres@radiancetech.com>
2018-10-10 12:29:41 -0500

    integration testing3

--- dockerservicemanager/handle_new_logs.go
+++ dockerservicemanager/handle_new_logs.go
@@ -30,7 +30,7 @@ func newLogger(ctx context.Context, dockerClient *client.Client, svc swarm.Servi
 			ShowStderr: true,
 			Follow:     true,
 		})
-		//defer logOut.Close()
+		//defer logOut.Close()  // before
 		if err != nil {
 			errs <- err
 			return
@@ -44,6 +44,8 @@ func newLogger(ctx context.Context, dockerClient *client.Client, svc swarm.Servi
 		} else if n == 0 {
 			errs <- fmt.Errorf("nothing read")
 		}
+
+		defer logOut.Close()
 
 		scanner := bufio.NewScanner(logOut)
 
