becad92dc2aac2a45ecf76b98420d5fbd51eac94
dan <dan@bauman.space>
2018-10-10 10:21:57 -0500

    timestamp to ms since 1971

--- customtypes/logs.go
+++ customtypes/logs.go
@@ -3,9 +3,9 @@ package customtypes
 // Log is a log with a container
 // name attached.
 type Log struct {
-	ContainerID   string  `json:"ContainerID,omitempty"`
-	ContainerName string  `json:"ContainerName,omitempty"`
-	Log           string  `json:"msg"`
-	ServiceName   string  `json:"sourceServiceName"`
-	LogTimestamp  float64 `json:"rt"`
+	ContainerID   string `json:"ContainerID,omitempty"`
+	ContainerName string `json:"ContainerName,omitempty"`
+	Log           string `json:"msg"`
+	ServiceName   string `json:"sourceServiceName"`
+	LogTimestamp  uint64 `json:"rt"`
 }
