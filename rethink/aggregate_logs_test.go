1125a6fa26ff4dfdd5246ef729dcbfcce1f2a27b
AlexT <alexander.torres@radiancetech.com>
2018-10-10 15:06:54 -0500

    integration testing16

--- rethink/aggregate_logs_test.go
+++ rethink/aggregate_logs_test.go
@@ -111,8 +111,8 @@ func Test_logSend(t *testing.T) {
 								continue L
 							}
 							if v, ok := doc["rt"]; ok {
-								now := uint64(time.Now().UnixNano() / (int64(time.Millisecond) / int64(time.Nanosecond)))
-								assert.True(t, (v.(uint64) >= now-10))
+								now := int64(time.Now().UnixNano()) /1000000
+								assert.True(t, (v.(int64) >= now-10))
 							} else {
 								continue L
 							}
